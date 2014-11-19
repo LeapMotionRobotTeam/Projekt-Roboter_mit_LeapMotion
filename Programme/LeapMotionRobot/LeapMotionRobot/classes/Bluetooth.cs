3e9b4d8fb538fcb45e286df0bba28b419f423ade
SimonBodner <simon.bodner@gmx.at>
2014-11-19 10:55:21 +0100

    LeapMotion C#-Programm

---
+++ Programme/LeapMotionRobot/LeapMotionRobot/classes/Bluetooth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapMotionRobot.classes
+{
+    class Bluetooth
+    {
+        private const string ARDUINOPORT = "COM6";
+
+        public SerialPort SerialPortArduino { get; set; }
+
+        public Bluetooth()
+        {
+            this.SerialPortArduino = new SerialPort(ARDUINOPORT);
+        }
+
+
+    }
+}
